c896f9904150757df41e577cd661937d44419858
TuralJSDeveloper <tural.azizxanli92@gmail.com>
2022-11-08 22:13:53 +0400

    Print Java Str

---
+++ JavaApplication28/src/javaapplication28/JavaApplication28.java
@@ -0,0 +1,15 @@
+package javaapplication28;
+
+/**
+ *
+ * @author Tural
+ */
+public class JavaApplication28 {
+
+    public static void main(String[] args) {
+        int a = 15;
+        System.out.println("Print Num:"+a);
+
+    }
+
+}
